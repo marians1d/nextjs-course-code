d4bcd1d4ea90ea5abf63b55b256309dd091e854f
Mariyan Damyanov <marians1md@gmail.com>
2022-12-28 11:58:07 +0200

    Adding links

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 function CoursesPage(params) {
     return (
         <div>
             <h1>Courses Page</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/courses/1">One</Link>
+                    </li>
+                    <li>
+                        <Link href="/courses/2">Two</Link>
+                    </li>
+                    <li>
+                        <Link href="/courses/3">Three</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 function HomePage() {
     return (
         <div>
             <h1>Home page</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/courses">Courses</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
